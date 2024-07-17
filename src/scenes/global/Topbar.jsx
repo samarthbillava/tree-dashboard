3714a7b76690cf8f8359231756d662a8cd25245c
samarthbillava <118583152+samarthbillava@users.noreply.github.com>
2024-07-18 00:15:10 +0530

    Add files via upload

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,41 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+
+import { tokens } from "../../theme";
+
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+       
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
